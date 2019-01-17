fce8f9ee52d47bdbe7a0178e99dca8671ede2b33
Viktor Petersson <vpetersson@wireload.net>
2019-01-17 11:15:05 +0000

    Tweaks how CERT_PATH is set

--- bin/start.sh
+++ bin/start.sh
@@ -5,11 +5,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-CERT_PATH=/data/wott/certs
-mkdir -p $CERT_PATH
-
 while true; do
-    CERT_PATH="$CERT_PATH" wott-agent
+    wott-agent
     python3 -c 'import agent; print("I am {}".format(agent.get_device_id()))'
     sleep 3600
 done
